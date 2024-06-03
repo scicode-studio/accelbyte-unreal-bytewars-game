98f3ec6ce63b93eeda10a9f3f71277e4cfd7b1d1
David Liebemann <david@scico.dev>
2024-06-03 22:25:39 +0200

    Basic bytewars project now works, started integrating odin

---
+++ Plugins/Odin/Source/Odin/Public/OdinSubsystem.h
@@ -0,0 +1,30 @@
+﻿/* Copyright (c) 2022-2024 4Players GmbH. All rights reserved. */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "odin_sdk.h"
+#include "Subsystems/EngineSubsystem.h"
+#include "OdinSubsystem.generated.h"
+
+class UOdinRoom;
+
+UCLASS()
+class ODIN_API UOdinSubsystem : public UEngineSubsystem
+{
+    GENERATED_BODY()
+  public:
+    static UOdinSubsystem* Get();
+
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+    virtual void Deinitialize() override;
+
+    void                      RegisterRoom(OdinRoomHandle RoomHandle, UOdinRoom* Room);
+    void                      DeregisterRoom(OdinRoomHandle RoomHandle);
+    TWeakObjectPtr<UOdinRoom> GetRoomByHandle(OdinRoomHandle RoomHandle);
+    bool                      IsRoomRegistered(OdinRoomHandle) const;
+
+  protected:
+    TMap<OdinRoomHandle, TWeakObjectPtr<UOdinRoom>> RegisteredRooms;
+    FCriticalSection                                RegisteredRoomsLock;
+};
